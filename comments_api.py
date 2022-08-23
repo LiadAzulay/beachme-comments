65a8d6250ece2128fabe53c6ff1de4bbded6cc20
LiadAzulay <azulayliad@gmail.com>
2022-08-23 09:31:58 -0700

    Adding comments api to heroku

---
+++ comments_api.py
@@ -0,0 +1,16 @@
+import pymongo
+from fastapi import FastAPI
+# Connecting to DB
+mydb = pymongo.MongoClient('mongodb+srv://user:user@beachme.c5sbvhv.mongodb.net/?retryWrites=true&w=majority')["beachme-1"]
+# Fetching "Comments"
+comments_db = mydb.comments
+# Creating api
+app = FastAPI()
+
+
+@app.get("/get_shore_comments/{id}")
+async def root(id):
+    comments = list(comments_db.find({"shore_id": int(id)}))
+    for c in comments:
+        c['_id'] = str(c['_id'])
+    return comments
